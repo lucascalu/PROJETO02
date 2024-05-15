24c4791123af9a5ee89a4c025cf28ffbe6485856
Lucas Schiavon Calu <54896910+lucascalu@users.noreply.github.com>
2024-05-15 10:57:54 -0300

    Add files via upload

---
+++ MANIPULARARDUINO/serialDesliga.ps1
@@ -0,0 +1,19 @@
+# Configurações da porta serial
+$portaSerial = New-Object System.IO.Ports.SerialPort
+$portaSerial.PortName = "COM30"
+$portaSerial.BaudRate = 9600
+
+# Abrir a porta serial
+$portaSerial.Open()
+
+# Dados a serem enviados
+$dados = "off"
+
+# Convertendo os dados em bytes e enviando
+$bytes = [System.Text.Encoding]::ASCII.GetBytes($dados)
+$portaSerial.Write($bytes, 0, $bytes.Length)
+
+# Fechar a porta serial após o envio
+$portaSerial.Close()
+
+Write-Host "Palavra 'on' enviada para a porta serial COM30 com sucesso."
